fa5115f019342f8940cef991fa0de42aa8a4e364
charlenne alexander <charlennep@gmail.com>
2020-06-08 14:55:21 -0500

    deployed application and make the connection

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
+// requiring dotenv 
+require("dotenv").config();
+
+// importing node packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
-
 const app = express();
 
+// parser middleware
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// static folder
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
 
+// port
+const PORT = 8000;
+
+// connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/under-control", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+// listener
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
